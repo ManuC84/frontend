28441ae72fb4d0c5b7e84b877995155495ce2a5f
Manu <mcufre84@gmail.com>
2021-04-04 22:06:55 +0200

    comments database refactored

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -3,6 +3,7 @@ import {
   createComment,
   hasError,
   startLoading,
+  fetchComments,
 } from "../reducers/slice/postsSlice";
 
 export const addComment = (id, payload) => async (dispatch) => {
@@ -10,6 +11,18 @@ export const addComment = (id, payload) => async (dispatch) => {
   try {
     const { data } = await API.addComments(id, payload);
     dispatch(createComment(data));
+  } catch (error) {
+    dispatch(hasError(error.response.data));
+  }
+};
+
+export const getComments = (id) => async (dispatch) => {
+  dispatch(startLoading);
+  try {
+    const { data } = await API.getComments(id);
+    if (data !== undefined) {
+      dispatch(fetchComments(data));
+    }
   } catch (error) {
     dispatch(hasError(error.response.data));
   }
