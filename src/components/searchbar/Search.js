4635d9bf9675d250d7f30b5e4d32131249ba4d23
Manu <mcufre84@gmail.com>
2021-04-22 18:17:38 +0200

    testing comma input in mobile

--- src/components/searchbar/Search.js
+++ src/components/searchbar/Search.js
@@ -122,7 +122,7 @@ const Search = () => {
   useEffect(() => {
     if (searchType === "tags") {
       const listener = (e) => {
-        if (e.code === "Comma" || e.code === "Tab") {
+        if (e.code === "Comma" || e.code === "Tab" || e.keyCode === 188) {
           if (tagButtonContent.length > 9) {
             setErrorMessage("Maximum of 10 tags allowed");
             setSearchError(true);
