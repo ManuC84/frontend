2b863abe8734223561ffa0e31a5ea2cc553326fc
Manu <mcufre84@gmail.com>
2021-04-29 15:30:23 +0200

    implementando grid en hero

--- src/components/about/hero/Hero.js
+++ src/components/about/hero/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import makeStyles from "./styles";
-import { Button, Container } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 // import phoneblob from "../../../img/phoneblob.png";
 import phoneblob from "../../../img/4.png";
 
@@ -9,22 +9,30 @@ const Hero = () => {
 
   return (
     <section className={classes.mainSection}>
-
-      <Container className={classes.heroContainer}>
-
-        {/* TITTLE + TEXT */}
-        <title className={classes.heroTextSection}>
-          <div className={classes.TitleSection}>¿Qué es FreelyComment?</div>
-          <div className={classes.Subtitol}>Es una APP gratuita donde podrás expresar tus sugerencias, opiniones y recomendaciones de cualquier sitio web, sin límites ni restricciones en un solo click.</div>
-          <Button className={classes.ColorButton}>¡ÚNETE YA!</Button>
-        </title>
-        
-        {/* IMAGE */}
-        <figure className={classes.heroImgSection}>
-          <img src={phoneblob} alt="phone" />
-        </figure>
-
-      </Container>
+      <Grid container style={{ padding: 20 }}>
+        <Grid item lg={6} md={6} sm={12} style={{ padding: 60 }}>
+          {/* TITTLE + TEXT */}
+          <title className={classes.heroTextSection}>
+            <div className={classes.TitleSection}>¿Qué es FreelyComment?</div>
+            <div className={classes.Subtitol}>
+              Es una APP gratuita donde podrás expresar tus sugerencias,
+              opiniones y recomendaciones de cualquier sitio web, sin límites ni
+              restricciones en un solo click.
+            </div>
+            <Button className={classes.ColorButton}>¡ÚNETE YA!</Button>
+          </title>
+        </Grid>
+        <Grid item lg={6} md={6} sm={12}>
+          {/* IMAGE */}
+          <figure>
+            <img
+              src={phoneblob}
+              alt="phone"
+              className={classes.heroImgSection}
+            />
+          </figure>
+        </Grid>
+      </Grid>
     </section>
   );
 };
--- src/components/about/hero/styles.js
+++ src/components/about/hero/styles.js
@@ -2,44 +2,49 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Autocomplete } from "@material-ui/lab";
 import background from "../../../img/backgroundHero.png";
 
-
 export default makeStyles((theme) => ({
-
   mainSection: {
     backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
     backgroundColor: "#ffff",
     // backgroundImage: "linear-gradient(to top, #64b5f6, #3c91e5, #226dd0, #2347b6, #311996);",
-
   },
 
-  heroContainer: {
-    display: "flex",
-    width: "90%",
-    display: "flex",
-    alignItems: "stretch",
-    height: "80vh",
-    paddinTop:"100vh",
-    
-  },
+  // heroContainer: {
+  //   display: "flex",
+  //   width: "90%",
+  //   display: "flex",
+  //   alignItems: "stretch",
+  //   height: "80vh",
+  //   paddinTop:"100vh",
+
+  // },
 
   heroTextSection: {
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
-    textAlign: "start",
-
   },
 
+  heroImgSection: {
+    [theme.breakpoints.down("sm")]: {
+      width: "70%",
+      height: "70%",
+    },
+  },
 
   TitleSection: {
-    fontFamily: 'MuseoModerno',
+    fontFamily: "MuseoModerno",
     fontSize: "2.5rem",
     fontWeight: 900,
     letterSpacing: "5px",
     color: "#0DDDC9",
-    paddingBottom:"20px",
+    paddingBottom: "20px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.8rem",
+    },
   },
 
   Subtitol: {
@@ -50,6 +55,9 @@ export default makeStyles((theme) => ({
     wordSpacing: 2,
     color: "#f8f9fa",
     paddingBottom: "30px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1rem",
+    },
   },
 
   ColorButton: {
@@ -63,10 +71,5 @@ export default makeStyles((theme) => ({
     "&:hover": {
       backgroundColor: "#7F8CE7",
     },
-    [theme.breakpoints.down("xs")]: {
-      display: "none",
-    },
   },
-
-
 }));
