d1d04eed7fdf4c384a85c436a04113d329360238
Manu <mcufre84@gmail.com>
2021-06-19 16:05:37 +0200

    change fab color back to secondary

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App(props) {
       <Footer />
 
       <ScrollTop {...props}>
-        <Fab color="primary" size="small" aria-label="scroll back to top">
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
         </Fab>
       </ScrollTop>
