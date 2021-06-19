f50ee15735a52059abdcc4e2eec6b0f75b3fd096
Manu <mcufre84@gmail.com>
2021-06-19 18:46:43 +0200

    fix auth alignment

--- src/pages/auth/styles.js
+++ src/pages/auth/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
   container: {
-    display: "flex",
+    display: "flex !important",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
