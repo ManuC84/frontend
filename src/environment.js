19c9a2411615f82b104c14f6610900f65922f802
Manuel Cufré <66978893+ManuC84@users.noreply.github.com>
2022-12-12 17:00:56 +0100

    Update backend URL

--- src/environment.js
+++ src/environment.js
@@ -3,7 +3,7 @@ const environment = {
 };
 
 if (process.env.REACT_APP_ENV === "staging") {
-  environment.baseUrl = "https://freelycomment.herokuapp.com/";
+  environment.baseUrl = "https://freelycomment-backend.onrender.com/";
 }
 
 export default environment;
